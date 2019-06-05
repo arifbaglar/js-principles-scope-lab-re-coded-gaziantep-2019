42cf0af70edd13ef83006c1de055c688771a533d
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-05 09:29:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 customerName='bob';
 function upperCaseCustomerName(){
-  var a=customerName.toUpperCase();
-  return a
+  return customerName.toUpperCase();
+  
 }
